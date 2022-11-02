86929ed6ff96f03e906e5f4447f72bfc6e0ff280
Innocent Niyonzima <89442975+Rwandantechy@users.noreply.github.com>
2022-11-02 23:27:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,46 @@
-let userForm = document.getElementById("user_form");
-var userEntries=[];
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+     var pos = 0; 
+    //our box element
+    var box = document.getElementById('box');
+    var x = document.getElementById('buutton');
+   // element.addEventListener("click", boarding);
 
-let errors=[]
-const retieveEntries = ()=>{
-    let entries = localStorage.getItem('userEntries')
-    if(entries){
-        entries=JSON.parse(entries)
-    }else{
-        entries=[]
+     //element.addEventListener("mouseover", boarding);
+    var t = setInterval(moveRight, 10);
+    //var t2 = setInterval(moveBottom, 20);
+  
+    function moveRight() {
+        if (pos >= 250)
+        {
+            clearInterval(t);
+        }
+        
+        else
+        {
+            pos += 1;
+            box.style.left = pos + 'px';
+            //(box.style.top = pos + 'px', 10);           
+        }
+        
     }
-    return entries
+    x.onclick = function () {
+        document.body.innerHTML = Date();
+    }
+}; 
+function change() {
+    var x = document.getElementById('name');
+    // x
+    if (x.value.length = 8) {
+        alert("Are you " + x.value);
+     
+        x.value =" your name is "+ x.value.toUpperCase();
+    }  
+
 }
-const displayEntries = ()=>{
-let entries=retieveEntries()
-const tbleEntries = entries.map((entry)=>{
-const nameCell = `<td class='border px-4 py-2'>${entry.FullName}</td>`
-const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`
-const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`
-const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`
-const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`
-const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`
-return row
-
-}).join('\n')
-const table =` <table class='table-auto w-full'>
-    <tr>
-    <th class='px-4 py-2 '>Name </th>
-    <th class='px-4 py-2 '>Email </th>
-    <th class='px-4 py-2 '>Password </th>
-    <th class='px-4 py-2 '>Dob </th>
-    <th class='px-4 py-2 '>Accepted terms? </th>
-    </tr>${tbleEntries}
-</table>`
-let details = document.getElementById('user-entries')
-details.innerHTML=table
-}
-
-const saveUserForm = (event)=>{
-event.preventDefault();
-const FullName = document.getElementById('name').value
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-const dob = document.getElementById('dob').value
-const acceptTerms = document.getElementById('acceptTerms').checked
-var currentYear = new Date().getFullYear();
-var birthYear = dob.split("-");
-let year=birthYear[0]
-var age = currentYear-year
-console.log({age,currentYear,birthYear})
-if(age < 18 || age > 55){
-    document.getElementById('dob').style='border:1px solid red'
-  return  alert("Age must be between 18 and 55")
-
-}else{
-    document.getElementById('dob').style='border:none'
-
-    const entry ={
-        FullName,
-        email,
-        password,
-        dob,
-        acceptTerms
-     }
-     userEntries=retieveEntries()
-     userEntries.push(entry);
-     localStorage.setItem("userEntries",JSON.stringify(userEntries))
-    displayEntries()
-    userForm.reset()
-   
-}
+// function boarding()
+//     {
+//         alert("certain event has been triggered on the document");
+//     }
  
-}
-userForm.addEventListener('submit',saveUserForm)
-displayEntries()
+
